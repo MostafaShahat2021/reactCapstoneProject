e9941a41e5f9203b10efe6e6f1b3ddea2aa167c5
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-09-07 11:54:10 +0300

    Update Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { FaMicrophone, FaAngleLeft, FaCog } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className="header-container">
-      <div className="angle-Left"><FaAngleLeft /></div>
+      <div className="angle-Left">
+        <Link to="/"><FaAngleLeft className="left" /></Link>
+      </div>
       <div className="cont">
-        <FaMicrophone />
-        <FaCog />
+        <FaMicrophone className="mic" />
+        <FaCog className="ger" />
       </div>
     </div>
   );
